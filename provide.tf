fa6782b4d4b88c728b7619a17dda53e4d4413fdb
Yuthika <yuthikavj@gmail.com>
2026-01-05 15:22:24 +0530

    provider.tf donew

---
+++ provide.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+    region = "ap-southeast-1"
+}
